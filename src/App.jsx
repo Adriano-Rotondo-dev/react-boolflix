38257f54f85179e10c6d893449e65412282ffea5
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-29 11:05:26 +0200

    add base navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,28 @@ import { useState } from "react";
 function App() {
   return (
     <>
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <form class="d-flex my-2 my-lg-0 ms-auto">
+              <input
+                class="form-control me-sm-2"
+                type="text"
+                placeholder="Search"
+              />
+              <button
+                class="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
       <h1>Hello Boolflix</h1>
     </>
   );
